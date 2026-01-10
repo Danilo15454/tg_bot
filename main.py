59530338a99c18e134a575656a2f6b9c455d5e47
Danila <planset980@gmail.com>
2026-01-10 21:51:40 +0200

    bot 0.1

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello world")
+import telebot
+from telebot import types
+
+TOKEN = "7854842729:AAGAyOEeo7T94TrbNN7LHr2xxowQiqD0DBY"
+bot = telebot.TeleBot(TOKEN)
+
+# Команда /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    # Создаём кнопку
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_hi = types.KeyboardButton("Сказать привет")
+    keyboard.add(button_hi)
+    
+    bot.send_message(message.chat.id, "Привет! Нажми кнопку ниже:", reply_markup=keyboard)
+
+# Обработчик нажатия кнопки
+@bot.message_handler(func=lambda message: message.text == "Сказать привет")
+def say_hi(message):
+    bot.send_message(message.chat.id, "👋 Привет!")
+
+# Запуск бота
+bot.polling()
