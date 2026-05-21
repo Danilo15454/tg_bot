0c7b04c64376594c44651ae4113033dfabf063ce
Danila <planset980@gmail.com>
2026-05-21 13:49:10 +0300

    35

--- educationCore/sirenReader.py
+++ educationCore/sirenReader.py
@@ -1,4 +1,7 @@
+from urllib import response
+
 from alerts_in_ua import Client as AlertsClient
+import requests
 
 class sirenReminder:
     def __init__(self, City:str, ApiKey:str):
@@ -9,4 +12,16 @@ class sirenReminder:
     def getData(self):
         request = self.Client.get_active_alerts()
         print(request)
+
+    def cityTake(self):
+        response = requests.get("https://ubilling.net.ua/aerialalerts/?source=skog&raw")
+    
+        data = response.json()
+
+        for district in data["raw"]["3"]["districts"]:
+            if district["name"] == "Дніпровський район":
+                print(district["alert"])
+                return district["alert"]
+        
+        
         
